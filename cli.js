3946fc3d9bbecff93215be131f46a28461e63b7f
Syed Faraaz Ahmad <faraaz98@live.com>
2019-06-30 15:50:53 +0530

    create cli file

--- cli.js
+++ cli.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+var argv = require('yargs')
+    .usage('Usage: $0 <command> [options]')
+    .command('create', 'Create the Floyd project in the specified path')
+    .example('$0 create -n example -p ~/projects', 'Create project folder example at ~/projects')
+    .alias('n', 'name')
+    .alias('p', 'path')
+    .nargs('f', 1)
+    .describe('f', 'Load a file')
+    .demandOption(['n', 'p'])
+    .help('h')
+    .alias('h', 'help')
+    .epilog('copyright 2019')
+    .argv;
